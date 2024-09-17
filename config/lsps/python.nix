90e92f329175d153a7851f3b12be575c282aa051
haq <haq@doesntexist.com>
2024-09-17 11:13:11 -0400

    whoops

--- config/lsps/python.nix
+++ config/lsps/python.nix
@@ -1,38 +1,6 @@
-{ pkgs, ... }: {
+{
   plugins = {
-    lsp = {
-      servers = {
-        pyright = {
-          enable = true;
-        };
-      };
-    };
-    none-ls = {
-      sources = {
-        formatting.black.enable = true;
-        diagnostics = { mypy.enable = true; };
-      };
-    };
+    lsp.servers.basedpyright.enable = true;
+    none-ls.sources.formatting.black.enable = true;
   };
-
-  extraPackages = with pkgs; [ 
-    sourcery
-  ];
-  # TODO: sops for token
-  extraConfigLua = # lua
-    ''
-      -- local lspconfig = require('lspconfig')
-      -- lspconfig.sourcery.setup {
-      --   -- Server-specific settings. See `:help lspconfig-setup`
-      --   settings = {
-      --     ['sourcery'] = {
-      --       init_options = {
-      --         token = "<to be placed by sops.nix>",
-      --         extension_version = "vim.lsp",
-      --         editor_version = "vim",
-      --       },
-      --     },
-      --   },
-      -- }
-    '';
 }
